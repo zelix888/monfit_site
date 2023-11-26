b9816470268f899d42ddfb6f36d0669d69b3e95d
zelix888 <zelix888@etik.com>
2023-11-26 12:34:51 +0100

    New Icons for geojson Cipriano track

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -9,36 +9,62 @@ if (document.getElementById(mapID2) !== null) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map2);
 
-  const start = L.icon({
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-  iconSize: [25, 41], // size of the icon
-  iconAnchor: [12, 41], // changed marker icon position
-  popupAnchor: [1, -34], // changed popup position
+  // Green Icon
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-icon-start.png',
+  shadowUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-shadow.png',
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [3, -34],
+  shadowSize: [41, 41]
 });
 
-const finish = L.icon({
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-    iconSize: [25, 41], // size of the icon
-    iconAnchor: [12, 41], // changed marker icon position
-    popupAnchor: [1, -34], // changed popup position
-  });
+// Blue Icon
+var blueIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-icon-wpt.png',
+  shadowUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-shadow.png',
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [3, -34],
+  shadowSize: [41, 41]
+});
 
-L.marker([45.07474205271855, 8.39181661605835], {
-    icon: start,
-  }).addTo(map2)
-
-  L.marker([45.08447772710751, 8.389872014522554], {
-    icon: finish,
-  }).addTo(map2)
+// Red Icon
+var redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-icon-end.png',
+  shadowUrl: 'https://raw.githubusercontent.com/zelix888/monfit_site/main/Images/Icons/pin-shadow.png',
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [3, -34],
+  shadowSize: [41, 41]
+});
     
   fetch("https://raw.githubusercontent.com/zelix888/monfit_site/main/routes/Cella_Monte/Cella%20Monte%20-%20Cipriano.geojson")
     .then((response) =>{
         return response.json()
     })
     .then((data) => {
-        L.geoJson(data).addTo(map2);
-    })
-    .catch((error) => {
-        console.log(`This is the error: ${error}`)
-    })
+      L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+          if (feature.properties.type == "from") {
+            return L.marker(latlng, {
+              icon: greenIcon 
+            });
+          } else if (feature.properties.type == "to") {
+            return L.marker(latlng, {
+              icon: redIcon 
+            });
+          } else if (feature.properties.type == "via") {
+            return L.marker(latlng, {
+              icon: blueIcon 
+            });
+          }
+          
+        }
+      }).bindPopup((layer) => {
+          return `${layer.feature.properties.name}`}).addTo(map2);
+  })
+  .catch((error) => {
+      console.log(`This is the error: ${error}`)
+  })
 }
